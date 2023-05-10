db5ed1be8ff1a90e500a88fce002cd2da02a82b2
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2023-05-10 20:22:39 +0700

    dimas(fix): fix stream data real time transmission

--- api_main/v1/controller.js
+++ api_main/v1/controller.js
@@ -4,154 +4,181 @@ const { getUser } = require("../../services/auth");
 const prisma = require("../../prisma/client");
 
 exports.startsesh = async (req, res) => {
-  try {
-    const device = await prisma.device.findUnique({
-      where: { userId: await getUser(req) },
-    });
-    console.log("device", device);
-    const status = await prisma.session.create({
-      data: {
-        active: true,
-        device: {
-          connect: {
-            id: device.id,
-          },
-        },
-      },
-    });
-    return resSuccess({ res, title: "Starting Session", data: { status } });
-  } catch (error) {
-    console.log(error);
-    return resError({ res, title: "Start Session Gagal", errors: error });
-  }
+    try {
+        const device = await prisma.device.findUnique({
+            where: { userId: await getUser(req) },
+        });
+        console.log("device", device);
+        const status = await prisma.session.create({
+            data: {
+                active: true,
+                device: {
+                    connect: {
+                        id: device.id,
+                    },
+                },
+            },
+        });
+        return resSuccess({ res, title: "Starting Session", data: { status } });
+    } catch (error) {
+        console.log(error);
+        return resError({ res, title: "Start Session Gagal", errors: error });
+    }
 };
 
 exports.stopsesh = async (req, res) => {
-  try {
-    const session = await prisma.session.findMany({
-      orderBy: {
-        id: "desc",
-      },
-      take: 1,
-    });
-    console.log("session", session);
-    const status = await prisma.session.update({
-      where: {
-        id: session[0].id,
-      },
-      data: {
-        active: false,
-      },
-    });
-    return resSuccess({ res, data: { status } });
-  } catch (error) {
-    console.log(error);
-    return resError({ res, title: "Stop Session Gagal", errors: error });
-  }
+    try {
+        const session = await prisma.session.findMany({
+            orderBy: {
+                id: "desc",
+            },
+            take: 1,
+        });
+        console.log("session", session);
+        const status = await prisma.session.update({
+            where: {
+                id: session[0].id,
+            },
+            data: {
+                active: false,
+            },
+        });
+        return resSuccess({ res, data: { status } });
+    } catch (error) {
+        console.log(error);
+        return resError({ res, title: "Stop Session Gagal", errors: error });
+    }
 };
 exports.streamdata = async (req, res) => {
-  try {
-    const { cahaya, jarak, flex, rest, deviceId } = req.body;
-
-    const session = await prisma.session.findFirst({
-      where: {
-        device: {
-          shortid: deviceId,
-        },
-        active: true,
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-      take: 1,
-      select: {
-        id: true,
-        device: {
-          select: {
-            shortid: true,
-          },
-        },
-      },
-    });
-
-    const lastData = await prisma.history.findFirst({
-      where: {
-        session: {
-          active: true,
-        },
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-
-    if (lastData === null) {
-      const historyData = await prisma.history.create({
-        data: {
-          ldr: cahaya,
-          distance: jarak,
-          posture: flex,
-          rest: rest,
-          session: {
-            connect: {
-              id: session.id,
+    try {
+        const { cahaya, jarak, flex, rest, deviceId } = req.body;
+        let historyData, text;
+        // Find Last Active Session
+        const session = await prisma.session.findFirst({
+            where: {
+                device: {
+                    shortid: deviceId,
+                },
+                active: true,
             },
-          },
-        },
-      });
-
-      return resSuccess({ res, title: "Stream Data", data: historyData });
-    }
-
-    if (lastData !== null) {
-      const timeDiff = (Date.now() - lastData.createdAt) / 1000 / 60;
-      if (timeDiff >= 1) {
-        const historyData = await prisma.history.create({
-          data: {
-            ldr: cahaya,
-            distance: jarak,
-            posture: flex,
-            rest: rest,
-            session: {
-              connect: {
-                id: session.id,
-              },
+            orderBy: {
+                createdAt: "desc",
             },
-          },
+            take: 1,
+            select: {
+                id: true,
+                device: {
+                    select: {
+                        shortid: true,
+                    },
+                },
+            },
         });
 
-        return resSuccess({ res, title: "Stream Data", data: historyData });
-      }
-    }
+        // Cari history, berdasarkan session id terakhir dan pastikan session tersebut masih aktif
+        const lastData = await prisma.history.findFirst({
+            where: {
+                session: {
+                    id: session.id,
+                    active: true,
+                },
+            },
+            orderBy: {
+                createdAt: "desc",
+            },
+            include: {
+                session: {
+                    include: {
+                        device: {
+                            include: {
+                                user: true,
+                            },
+                        },
+                    },
+                },
+            },
+        });
 
-    req.app.io.emit(`onbroadcast/${session.device.shortid}`, {
-      cahaya,
-      jarak,
-      flex,
-      rest,
-    });
-  } catch (error) {
-    console.log(error);
-    return resError({ res, title: "Stream Data failed", errors: error });
-  }
+        // Jika belum terdapat data history, maka langsung masukan data tersebut ke database
+        if (lastData === null) {
+            historyData = await prisma.history.create({
+                data: {
+                    ldr: cahaya,
+                    distance: jarak,
+                    posture: flex,
+                    rest: rest,
+                    session: {
+                        connect: {
+                            id: session.id,
+                        },
+                    },
+                },
+            });
+
+            text =
+                "Berhasil menerima data pertama, sukses menyimpan data ke database";
+        }
+
+        // Jika Sudah terdapat data history
+        if (lastData !== null) {
+            // Pastikan data yang akan disimpan berjarak 1 menit dari data sebelumnya
+            const timeDiff = (Date.now() - lastData.createdAt) / 1000 / 60;
+            if (timeDiff >= 1) {
+                historyData = await prisma.history.create({
+                    data: {
+                        ldr: cahaya,
+                        distance: jarak,
+                        posture: flex,
+                        rest: rest,
+                        session: {
+                            connect: {
+                                id: session.id,
+                            },
+                        },
+                    },
+                });
+
+                text = "Data berhasil disimpan ke database";
+            }
+        }
+
+        req.app.io.emit(`onbroadcast/${session.device.shortid}`, {
+            cahaya,
+            jarak,
+            flex,
+            rest,
+        });
+
+        return resSuccess({
+            res,
+            title: "Sukses Menerima data, data tidak disimpan ke database",
+        });
+    } catch (error) {
+        console.log(error);
+        return resError({ res, title: "Stream Data failed", errors: error });
+    }
 };
 
 module.exports.summaryTime = async (req, res) => {
-  try {
-    const userId = await getUser(req);
-    const lastSession = await prisma.session.findFirst({
-      where: {
-        device: {
-          userId: userId,
-        },
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+    try {
+        const userId = await getUser(req);
+        const lastSession = await prisma.session.findFirst({
+            where: {
+                device: {
+                    userId: userId,
+                },
+            },
+            orderBy: {
+                createdAt: "desc",
+            },
+        });
 
-    return resSuccess({ res, title: "Using Time Success!", data: lastSession });
-  } catch (error) {
-    console.log(error.message);
-  }
+        return resSuccess({
+            res,
+            title: "Using Time Success!",
+            data: lastSession,
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
 };
